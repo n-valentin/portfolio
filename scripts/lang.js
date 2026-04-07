3da0612c08b5fd049e3f03094cfbe3e2119c281f
val <_@_>
2026-04-07 10:38:59 +0200

    v1

---
+++ scripts/lang.js
@@ -0,0 +1,85 @@
+let current = "en"; // switched on load
+
+const translations = {
+    da: {
+        button: "Switch to English 🇺🇸",
+        h0: "Datatekniker — Programmering",
+
+        h1: "Profil",
+        p1: "Stille type med ADHD, hvilket giver mig ekstremt fokus og energi, når jeg arbejder med noget jeg brænder for.",
+        p2: "Jeg lærer hurtigt, arbejder bedst selvstændigt med fordybelse og tekniske udfordringer alene, men bidrager også gerne i mindre samarbejdsgrupper.",
+        p3: "Jeg fortækker ikke at arbejde med AI til problemløsning.",
+
+        h2: "Uddannelse",
+
+        h3: "Kompetencer",
+        ul1: "Kørekort til Kategori-B",
+        ul2: "Lærenem og idérig",
+        ul3: "Best med Lua og C#",
+        ul4: "Spilsikkerhed og Modding",
+        ul5: "Deklarativ programmering",
+        ul6: "Stærk i Teknik og Datalogi",
+        ul7: "Stærk i Pogrammeringsteori",
+        ul8: "Stærk i Engelsk og Matematik",
+
+        h4: "Erfaring",
+        ul9: "Mange år som Discord server administrator",
+        ul10: "C#: Discord Bots med .NET",
+        ul11: "F#: Autofishing macro for Roblox spil",
+        ul12: "AI: Brug af Ollama — lokal LLM",
+        ul13: "BepInEx: Unity Modding — VR spil",
+        ul14: "Roblox Lua: Flere Camera scripts",
+        ul15: "Roblox Lua: Genbrug af Quake's 'bunnyhop kode' med Vector Projection",
+        ul16: "Lua: Garry's Mod Expression 2 og Starfall",
+
+        h5: "Kendskaber",
+        ul17: "Netværk, Subnetting, Kommunikationsteori",
+        ul18: "Kendskab til AI siden 2021",
+    },
+    en: {
+        button: "Skift til Dansk 🇩🇰",
+        h0: "Datatechnician — Programming",
+
+        h1: "Profile",
+        p1: "Chill person with ADHD, which gives me great focus and energy, when I work with something I enjoy.",
+        p2: "I'm a fast learner, working best independently when immersed with technical challenges alone, but willing to contribute in groups.",
+        p3: "Personally against use of AI when it comes to problemsolving.",
+
+        h2: "Education",
+
+        h3: "Capabilities",
+        ul1: "Drivers' License",
+        ul2: "Fast learner and Innovative",
+        ul3: "Best with Lua and C#",
+        ul4: "Game-security og Modding",
+        ul5: "Declarative orogramming",
+        ul6: "Strong in general tech and computer science",
+        ul7: "Strong in Programming theory",
+        ul8: "Strong in English and Math",
+
+        h4: "Experience",
+        ul9: "Many years as Discord server administrator",
+        ul10: "C#: Discord Bots with .NET",
+        ul11: "F#: Autofishing macro for a Roblox game",
+        ul12: "AI: Use of Ollama — local LLM",
+        ul13: "BepInEx: Unity Modding — VR games",
+        ul14: "Roblox Lua: Multiple Camera scripts",
+        ul15: "Roblox Lua: Remaking Quake's 'bunnyhop code' with Vector Projection",
+        ul16: "Lua: Garry's Mod Expression 2 and Starfall",
+
+        h5: "Familiarity",
+        ul17: "Networking, Communication-theory",
+        ul18: "Familiar with AI since 2021",
+
+    }
+};
+
+function toggle() {
+    current = current === "da" ? "en" : "da";
+
+    //update
+    document.querySelectorAll("[data-key]").forEach(el => {
+    const key = el.getAttribute("data-key");
+    el.textContent = translations[current][key];
+  });
+}
